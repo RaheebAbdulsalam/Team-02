f16ad21569765a257f462ad5c560a1aa05332913
Raheeb Abdulsalam <raheebnomery2013@hotmail.com>
2023-02-15 11:02:40 +0000

    Updated the controller

---
+++ LoginSystem/LoginSystem/src/main/java/GameStationLoginSystem/com/service/CustomUserDetailsService.java
@@ -0,0 +1,30 @@
+package GameStationLoginSystem.com.service;
+
+
+import GameStationLoginSystem.com.model.CustomUserDetails;
+import GameStationLoginSystem.com.model.User;
+import GameStationLoginSystem.com.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepo;
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepo.findByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return new CustomUserDetails(user);
+    }
+    }
+
+
+
+
