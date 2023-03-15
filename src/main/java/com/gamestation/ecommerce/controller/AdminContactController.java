ad2726e20059043fbdea789992e7f2b8419b4745
Raheeb Abdulsalam <raheebnomery2013@hotmail.com>
2023-03-15 23:09:32 +0000

    Added a html page to display messges in the Admin Panel

--- src/main/java/com/gamestation/ecommerce/controller/AdminContactController.java
+++ src/main/java/com/gamestation/ecommerce/controller/AdminContactController.java
@@ -8,11 +8,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
-@Controller
+@RestController
+@RequestMapping("/admin/messages")
 public class AdminContactController {
 
     @Autowired
