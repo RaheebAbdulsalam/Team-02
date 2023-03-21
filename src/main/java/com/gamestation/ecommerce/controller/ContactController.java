1992e9406b3be5d581a7be6139f887e524da79a9
Raheeb Abdulsalam <raheebnomery2013@hotmail.com>
2023-03-21 21:26:37 +0000

    Added a page with styling which displays success message when user send a message

--- src/main/java/com/gamestation/ecommerce/controller/ContactController.java
+++ src/main/java/com/gamestation/ecommerce/controller/ContactController.java
@@ -3,11 +3,14 @@ package com.gamestation.ecommerce.controller;
 import com.gamestation.ecommerce.model.Contact;
 import com.gamestation.ecommerce.repository.ContactRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.security.Principal;
 
 @RestController
 public class ContactController {
@@ -26,5 +29,11 @@ public class ContactController {
         contactRepository.save(contact);
         return new ModelAndView("contact");
         //return new ModelAndView("success"); // success page
+    }
+
+    @GetMapping("/successMessage")
+    public ModelAndView viewSuccessMessage(Model model, Principal principal) {
+        ModelAndView mav = new ModelAndView("successMessage");
+        return mav;
     }
 }
