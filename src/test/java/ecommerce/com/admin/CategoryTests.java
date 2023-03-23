81a6e271f301f5aae838658084ceedcc524d9b53
bucephalus26 <ibrahimkhan13@hotmail.com>
2023-03-23 14:04:28 +0000

    updating product test to include search query

--- src/test/java/ecommerce/com/admin/CategoryTests.java
+++ src/test/java/ecommerce/com/admin/CategoryTests.java
@@ -9,8 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
-import org.springframework.mock.web.MockMultipartFile;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
---
+++ src/test/java/ecommerce/com/admin/ProductTests.java
@@ -0,0 +1,102 @@
+package ecommerce.com.admin;
+
+import com.gamestation.ecommerce.EcommerceApplication;
+import com.gamestation.ecommerce.exception.ResourceNotFoundException;
+import com.gamestation.ecommerce.model.Category;
+import com.gamestation.ecommerce.model.Product;
+import com.gamestation.ecommerce.repository.CategoryRepository;
+import com.gamestation.ecommerce.service.CategoryService;
+import com.gamestation.ecommerce.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductTests {
+
+    @Autowired
+    private ProductService productService;
+
+    // Test for creating a product
+    @Test
+    public void testCreateProduct() {
+        Product product = new Product();
+        product.setName("Test Product");
+        product.setPrice(new BigDecimal("10.00"));
+        product.setDescription("This is a test product");
+        product.setStockLevel(5);
+        List<Category> categories = new ArrayList<>();
+        Category category = new Category();
+        category.setName("Test Category");
+        categories.add(category);
+        product.setCategories(categories);
+
+        Product savedProduct = productService.createProduct(product);
+
+        assertNotNull(savedProduct);
+        assertNotNull(savedProduct.getId());
+        assertEquals("Test Product", savedProduct.getName());
+        assertEquals(new BigDecimal("10.00"), savedProduct.getPrice());
+        assertEquals("This is a test product", savedProduct.getDescription());
+        assertEquals(Integer.valueOf(5), savedProduct.getStockLevel());
+        assertNotNull(savedProduct.getImage());
+        assertFalse(savedProduct.getCategories().isEmpty());
+        assertEquals("Test Category", savedProduct.getCategories().get(0).getName());
+    }
+
+    // retrieving products
+    @Test
+    public void testGetAllProducts() {
+        List<Product> products = productService.getAllProducts();
+
+        assertNotNull(products);
+        assertFalse(products.isEmpty());
+    }
+
+    // retrieving specific product
+    @Test
+    public void testGetProductById() {
+        Product product = productService.getProductById(1);
+
+        assertNotNull(product);
+        assertEquals("Product 1", product.getName());
+    }
+
+    // updating specific product
+    @Test
+    public void testUpdateProduct() {
+        Product product = productService.getProductById(1);
+        product.setName("Updated Product");
+        product.setPrice(new BigDecimal("20.00"));
+        product.setDescription("This is an updated product");
+        product.setStockLevel(10);
+
+        Product updatedProduct = productService.updateProduct(1, product);
+
+        assertNotNull(updatedProduct);
+        assertEquals("Updated Product", updatedProduct.getName());
+        assertEquals(new BigDecimal("20.00"), updatedProduct.getPrice());
+        assertEquals("This is an updated product", updatedProduct.getDescription());
+        assertEquals(Integer.valueOf(10), updatedProduct.getStockLevel());
+    }
+
+    // deleting specific product
+    @Test
+    public void testDeleteProduct() {
+        productService.deleteProduct(1);
+        assertNull(productService.getProductById(1));
+    }
+
+    // testing search function
+    @Test
+    public void testSearch() {
+        List<Product> products = productService.search("Product");
+
+        assertNotNull(products);
+        assertFalse(products.isEmpty());
+    }
+}
